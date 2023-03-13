396395ec9f314654a4366b9819d493aeb50523b3
Shavronskiy Dima <dima.shavronskyi@gmail.com>
2023-03-13 16:13:33 +0200

    Change backgroud, add search box(need to fix)

---
+++ SocialNetworkAnalyzer/Models/InstagramUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SocialNetworkAnalyzer.Models
+{
+    public class InstagramUser
+    {
+        public string userName { get; set; }
+        public string userFollowers { get; set; }
+        public string userDescription { get; set; }
+        public string userImg { get; set; }
+    }
+}
